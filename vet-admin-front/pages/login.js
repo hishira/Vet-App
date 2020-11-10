7d944cd0b2d06b874019cc4759d5c33522b5b6ee
hishira <okami0124@gmail.com>
2020-11-10 01:05:55 +0100

    Add login input, and stylized something in login view

--- vet-admin-front/pages/login.js
+++ vet-admin-front/pages/login.js
@@ -1,5 +1,31 @@
+import {useState} from 'react'
+import style from '../styles/Login.module.css'
+
 export default function Login(){
-    return<div>
-        Login plce
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const loginHandle = ()=>{
+        let documentelement = document.getElementsByClassName(style.loginButton)[0];
+        documentelement.classList.add(style.loginbuttonanimate)
+        setTimeout(()=>
+            documentelement.classList.remove(style.loginbuttonanimate)
+        ,800)
+        console.log(email,password);
+    }
+    return<div className={style.maincontainer}>
+        <main className={style.maincontent}>
+            <p className={style['maincontent-title']}>
+                    Login
+            </p>
+            <div className={style.inputcontainer}>
+                <label  className={style.labelclass} >Email:</label>
+                <input onChange ={(e)=>setEmail(e.target.value)} type='text' className={style.inputclass}/>
+                <label className={style.labelclass} >Password:</label>
+                <input onChange={(e)=>setPassword(e.target.value)}  type='password' className={style.inputclass}/>
+            </div>
+            <div className={style.buttoncontainer}>
+            <button className={style.loginButton} onClick={()=> loginHandle()}>Login</button>
+            </div>
+        </main>
     </div>
 }
