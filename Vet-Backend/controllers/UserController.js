3ef3ec8c22bc50dad21724de2f5f08241d7b864f
hishira <okami0124@gmail.com>
2020-11-17 23:29:24 +0100

    Create chane email and password functions

--- Vet-Backend/controllers/UserController.js
+++ Vet-Backend/controllers/UserController.js
@@ -3,27 +3,28 @@ const admin = require("firebase-admin");
 class UserController {
   static async createUser(req, res) {
     try {
-        let uid = ""
-        uid = await admin
+      let uid = "";
+      uid = await admin
         .auth()
         .createUser({
           email: req.body.email,
           password: req.body.password,
           emailVerified: false,
           disabled: false,
-        }).then((userRecord)=>{
-            return userRecord.uid
+        })
+        .then((userRecord) => {
+          return userRecord.uid;
         });
-        console.log(uid)
-        const user = new userModel({
-            userID: uid,
-            email:req.body.email,
-            type: req.body.userTYPE ? req.body.userTYPE : "USER",
-          });
-          await user.save();
-          return res.status(200).json(user);
+      console.log(uid);
+      const user = new userModel({
+        userID: uid,
+        email: req.body.email,
+        type: req.body.userTYPE ? req.body.userTYPE : "USER",
+      });
+      await user.save();
+      return res.status(200).json(user);
     } catch (e) {
-        console.log(e.message)
+      console.log(e.message);
       return res.status(404).send(e.message);
     }
   }
@@ -40,6 +41,40 @@ class UserController {
       const users = await userModel.find({});
       return res.status(200).json(users);
     } catch (e) {
+      return res.status(404).send("Server error");
+    }
+  }
+  static async emailChange(req, res) {
+    try {
+      await admin
+        .auth()
+        .updateUser(req.body.uid, {
+          email: req.body.email,
+        })
+        .then((userRecord) => console.log("OK"))
+        .catch((error) => console.log(error));
+      const user = await userModel.findByIdAndUpdate(
+        req.body.userID,
+        { email: req.body.email },
+        { new: true },
+        (err, model) => {
+          if (err) return res.status(404).send("Error while updating");
+          return res.status(200).json(model);
+        }
+      );
+    } catch (e) {
+      return res.status(404).send("Server error");
+    }
+  }
+  static async passwordChange(req, res) {
+    try {
+      await admin
+        .auth()
+        .updateUser(req.body.uid, { password: req.body.password })
+        .then((userRecors) => console.log("Ok"))
+        .catch((err) => console.log(err));
+      return res.status(200).json({message:"OK"});
+    } catch (err) {
       return res.status(404).send("Server error");
     }
   }
