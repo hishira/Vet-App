c4dd3855f1b129bde2f3d6cd64a64c25958a057d
hishira <okami0124@gmail.com>
2020-11-22 00:05:04 +0100

    Add view for visit more info

--- vet-admin-front/pages/user/admin/allvisits/[id].js
+++ vet-admin-front/pages/user/admin/allvisits/[id].js
@@ -4,8 +4,9 @@ import { useRouter } from "next/router";
 import { getInfoAboutVisit } from "../../../../utils/api/visitApi";
 import { getUserFromCookie } from "../../../../utils/auth/userCookies";
 import Loading from "../../../../components/loader";
+import styles from "../../../../styles/UsersVisits.module.css";
 export default function VisitById(props) {
-  const [visitInfo,setVisitInfo] = useState({});
+  const [visitInfo, setVisitInfo] = useState({});
   const [loading, setLoading] = useState("no");
   const router = useRouter();
   const { id } = router.query;
@@ -23,8 +24,9 @@ export default function VisitById(props) {
         });
         if (data === false) throw new Error("error");
         setVisitInfo(data);
+        console.log(data);
         setLoading("end");
-        } catch (e) {
+      } catch (e) {
         setLoading("error");
       }
     };
@@ -35,7 +37,25 @@ export default function VisitById(props) {
       {loading === "yes" ? (
         <Loading />
       ) : loading === "end" ? (
-        <div>{JSON.stringify(visitInfo)}</div>
+        <div>
+          <div className={styles.moreinfocomponent}>
+            <div className={styles.wheninfo}>
+              Date: {`${visitInfo.when.split("T")[0]} at ${visitInfo.time}`}
+            </div>
+            <div className={styles.whereinfo}>
+              {`Where: ${visitInfo.clinic.city}, ${visitInfo.clinic.address}`}
+            </div>
+            <div className={styles.petinfo}>
+              {`Pet: ${visitInfo.pet.species}, Name: ${visitInfo.pet.name}, Age:${visitInfo.pet.age}`}
+            </div>
+          </div>
+          <div
+            onClick={() => router.push("/user/admin/allvisits")}
+            className={styles.arrowcontainer}
+          >
+            <div className={styles.leftarrow}></div>
+          </div>
+        </div>
       ) : loading === "error" ? (
         <div>Error</div>
       ) : (
