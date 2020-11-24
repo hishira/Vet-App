d9628723979624dec433ae7ade6136d405ebd4b2
hishira <okami0124@gmail.com>
2020-11-24 18:01:30 +0100

    Now admin can delete pets

--- vet-admin-front/pages/user/admin/allpets/index.js
+++ vet-admin-front/pages/user/admin/allpets/index.js
@@ -1,13 +1,17 @@
 import { useState, useEffect } from "react";
 import UserView from "../../index";
 import styles from "../../../../styles/UserPets.module.css";
-import { getPetBySpecies } from "../../../../utils/api/petApi";
+import { getPetBySpecies,deletePet } from "../../../../utils/api/petApi";
 import { getUserFromCookie } from "../../../../utils/auth/userCookies";
 import Loader from "../../../../components/loader";
+import YesOrNoDialog from '../../../../components/yesornodialoguser';
 export default function AllPets(props) {
   const [petType, selectPetType] = useState("Dog");
   const [pets, setPets] = useState([]);
   const [loading, setLoading] = useState("false");
+  const [reload,setReload] = useState(false);
+  const [petToDelete,setPetDoDelete] = useState({});
+  const [deleteDialogOpen,setDeleteDialogOpen] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
       let obj = { species: petType };
@@ -26,10 +30,39 @@ export default function AllPets(props) {
       }
     };
     if (petType !== "") fetchData();
-  }, [petType]);
+  }, [petType,reload]);
+  const beforePetDelete = (pet)=>{
+    setPetDoDelete(pet);
+    setDeleteDialogOpen(!deleteDialogOpen);
+  }
+  const closeDeleteDialog = ()=>{
+    setDeleteDialogOpen(!deleteDialogOpen);
+  }
+  const deletePetHandle = async ({_id})=>{
+    let obj = {
+      petID: _id
+    }
+    console.log(obj);
+    const token = getUserFromCookie()["token"]
+    let data = await deletePet(obj,token).then(response=>{
+      if(response.status === 200)
+        return true;
+      return false;
+    })
+    if(data){
+      setReload(!reload);
+    }
+  }
   return (
     <UserView userdata={props.userdata}>
       <div className={styles.maincomponent}>
+        <YesOrNoDialog
+          handleChange={closeDeleteDialog}
+          newuser={petToDelete}
+          open={deleteDialogOpen}
+          message="Are you sure to delete pet"
+          yeshandle={deletePetHandle}
+          />
         <label className={styles.label}>
           <div>Select pet:</div>
           <select
@@ -51,14 +84,14 @@ export default function AllPets(props) {
         ) : loading === "end" ? (
           <div className={styles.maincomponent}>
             {pets.map((pet) => (
-              <div className={styles.petcomponent}>
+              <div key={pet._id} className={styles.petcomponent}>
                 
                   <div className={styles.petinfo}>
                     <div>Name: {pet.name}</div>
                     <div>Age: {pet.age}</div>
                   </div>
                   <div className={styles.petbuttons}>
-                    <button className={styles.deletebutton}>Delete</button>
+                    <button className={styles.deletebutton} onClick={()=>beforePetDelete(pet)}>Delete</button>
                     <button className={styles.editbutton}>Edit</button>
                   </div>
                 
--- vet-admin-front/utils/api/petApi.js
+++ vet-admin-front/utils/api/petApi.js
@@ -4,4 +4,8 @@ async function getPetBySpecies(obj, token = "") {
   let url = getApiLink("pet/getpetbyspecies");
   return await fetch(url, GetFetchPostObjectWithAuth(obj, token));
 }
-export { getPetBySpecies };
+async function deletePet(obj,token=""){
+  let url = getApiLink("pet/deletepet");
+  return await fetch(url,GetFetchPostObjectWithAuth(obj,token));
+}
+export { getPetBySpecies,deletePet };
