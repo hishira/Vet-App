bb31f3b20756d55879d72153aae1c6266646bcd9
hishira <okami0124@gmail.com>
2020-12-21 19:42:38 +0100

    Refactor all pets view

--- vet-admin-front/pages/user/admin/allpets/index.js
+++ vet-admin-front/pages/user/admin/allpets/index.js
@@ -1,72 +1,80 @@
 import { useState, useEffect } from "react";
 import UserView from "../../index";
 import styles from "../../../../styles/UserPets.module.css";
-import { getPetBySpecies,deletePet } from "../../../../utils/api/petApi";
+import { getPetBySpecies, deletePet } from "../../../../utils/api/petApi";
 import { getUserFromCookie } from "../../../../utils/auth/userCookies";
 import Loader from "../../../../components/loader";
-import YesOrNoDialog from '../../../../components/yesornodialoguser';
-import EditPetModal from '../../../../components/EditPetModal';
+import YesOrNoDialog from "../../../../components/yesornodialoguser";
+import EditPetModal from "../../../../components/EditPetModal";
 export default function AllPets(props) {
   const [petType, selectPetType] = useState("Dog");
   const [pets, setPets] = useState([]);
   const [loading, setLoading] = useState("false");
-  const [reload,setReload] = useState(false);
-  const [petToDelete,setPetDoDelete] = useState({});
-  const [deleteDialogOpen,setDeleteDialogOpen] = useState(false);
+  const [reload, setReload] = useState(false);
+  const [petToDelete, setPetDoDelete] = useState({});
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
+
+  const [petToEdit, setPetToEdit] = useState({});
+  const [petEditDialogOpen, setEditDialogOpen] = useState(false);
   
-  const [petToEdit,setPetToEdit] = useState({});
-  const [petEditDialogOpen,setEditDialogOpen] = useState(false);
+  const getPetsBySpecies = async (objectToSend,userToken) =>{
+    let pets = await getPetBySpecies(objectToSend, userToken).then((response) => {
+      if (response.status === 200) return response.json();
+      return false;
+    });
+    if (pets === false) throw new Error("error");
+    return pets
+  }
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchPetData = async () => {
       let obj = { species: petType };
       try {
         const token = getUserFromCookie()["token"];
+        let petsbyspecies = await getPetsBySpecies(obj,token);
         setLoading("yes");
-        let data = await getPetBySpecies(obj, token).then((response) => {
-          if (response.status === 200) return response.json();
-          return false;
-        });
-        if (data === false) throw new Error("error");
-        setPets(data);
+        setPets(petsbyspecies);
         setLoading("end");
       } catch (e) {
         setLoading("error");
       }
     };
-    if (petType !== "") fetchData();
-  }, [petType,reload]);
-  const beforePetDelete = (pet)=>{
+    if (petType !== "") fetchPetData();
+  }, [petType, reload]);
+  const beforePetDelete = (pet) => {
     setPetDoDelete(pet);
     setDeleteDialogOpen(!deleteDialogOpen);
-  }
-  const closeDeleteDialog = ()=>{
+  };
+  const closeDeleteDialog = () => {
     setDeleteDialogOpen(!deleteDialogOpen);
-  }
-  const reloadhandle = ()=>{
+  };
+  const reloadhandle = () => {
     setReload(!reload);
-  }
-  const deletePetHandle = async ({_id})=>{
-    let obj = {
-      petID: _id
-    }
-    console.log(obj);
-    const token = getUserFromCookie()["token"]
-    let data = await deletePet(obj,token).then(response=>{
-      if(response.status === 200)
-        return true;
+  };
+  const deletepetbyid = async (petid, usertoken) => {
+    let objecttosend = {
+      petID: petid,
+    };
+    let deletedPet = await deletePet(objecttosend, usertoken).then((response) => {
+      if (response.status === 200) return true;
       return false;
-    })
-    if(data){
+    });
+    if (deletedPet === false) throw new Error("Error");
+  };
+  const deletePetHandle = async ({ _id }) => {
+    
+    const token = getUserFromCookie()["token"];
+    let petdeleted = deletepetbyid(_id, token);
+    if (petdeleted) {
       setReload(!reload);
     }
-  }
-  const editPetHandle = (pet)=>{
+  };
+  const editPetHandle = (pet) => {
     setPetToEdit(pet);
     setEditDialogOpen(!petEditDialogOpen);
-  }
-  const closeEditDialogHandle = ()=>{
+  };
+  const closeEditDialogHandle = () => {
     setEditDialogOpen(!petEditDialogOpen);
-  }
+  };
   return (
     <UserView userdata={props.userdata}>
       <div className={styles.maincomponent}>
@@ -76,13 +84,13 @@ export default function AllPets(props) {
           open={deleteDialogOpen}
           message="Are you sure to delete pet"
           yeshandle={deletePetHandle}
-          />
-          <EditPetModal
-            open={petEditDialogOpen}
-            pet={petToEdit}
-            reloadhandle={reloadhandle}
-            closehandle={closeEditDialogHandle}
-          />
+        />
+        <EditPetModal
+          open={petEditDialogOpen}
+          pet={petToEdit}
+          reloadhandle={reloadhandle}
+          closehandle={closeEditDialogHandle}
+        />
         <label className={styles.label}>
           <div>Select pet:</div>
           <select
@@ -105,16 +113,24 @@ export default function AllPets(props) {
           <div className={styles.pets}>
             {pets.map((pet) => (
               <div key={pet._id} className={styles.petcomponent}>
-                
-                  <div className={styles["petcomponent__petinfo"]}>
-                    <div>Name: {pet.name}</div>
-                    <div>Age:  {pet.age}</div>
-                  </div>
-                  <div className={styles.petbuttons}>
-                    <button className={styles["petbuttons__deletebutton"]} onClick={()=>beforePetDelete(pet)}>Delete</button>
-                    <button className={styles["petbuttons__editbutton"]} onClick={()=>editPetHandle(pet)}>Edit</button>
-                  </div>
-                
+                <div className={styles["petcomponent__petinfo"]}>
+                  <div>Name: {pet.name}</div>
+                  <div>Age: {pet.age}</div>
+                </div>
+                <div className={styles.petbuttons}>
+                  <button
+                    className={styles["petbuttons__deletebutton"]}
+                    onClick={() => beforePetDelete(pet)}
+                  >
+                    Delete
+                  </button>
+                  <button
+                    className={styles["petbuttons__editbutton"]}
+                    onClick={() => editPetHandle(pet)}
+                  >
+                    Edit
+                  </button>
+                </div>
               </div>
             ))}
           </div>
